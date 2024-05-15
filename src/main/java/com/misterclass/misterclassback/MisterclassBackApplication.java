4d47894eeb4cb5d2047ab7ebd9a78476643ac93e
Ginés de Gea <gines@example.com>
2024-05-15 18:36:19 +0200

    feat: first commit

---
+++ src/main/java/com/misterclass/misterclassback/MisterclassBackApplication.java
@@ -0,0 +1,13 @@
+package com.misterclass.misterclassback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisterclassBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisterclassBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/misterclass/misterclassback/MisterclassBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misterclass.misterclassback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisterclassBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
